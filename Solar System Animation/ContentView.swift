d1062507d43e12636e3f791d67736b434738a363
Daluven <damienlau2006@gmail.com>
2022-06-18 10:54:58 +0800

    Initial Commit

---
+++ Solar System Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Solar System Animation
+//
+//  Created by T Krobot on 18/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Solar System Animation/Solar_System_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_System_AnimationApp.swift
+//  Solar System Animation
+//
+//  Created by T Krobot on 18/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Solar_System_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
